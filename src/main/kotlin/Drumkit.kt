556eaae21118bf97050da662235411e248ef34c9
Jörn Smock <jsmock@posteo.de>
2021-03-02 23:48:26 +0100

    Didn't really get it but here's the final version in appendix 1 of the Head First Kotlin book.

--- src/main/kotlin/Drumkit.kt
+++ src/main/kotlin/Drumkit.kt
@@ -1,20 +1,34 @@
-/* Here HFKotlin uses GlobalScope.launch to create thread. */
+/* Here HFKotlin tries to explain
+ * - globalScope.launch vs. launch
+ * - threads vs. coroutines
+ * - delay suspends only this coroutine
+ * - ways to start things and let them pause
+ *
+ * ...but do I get it???
+ */
+
 import java.io.File
 import javax.sound.sampled.AudioSystem
 import kotlinx.coroutines.*
 
-fun playBeats(beats: String, file: String) {
+// keyword for the compiler: this can be suspended
+suspend fun playBeats(beats: String, file: String) {
     val parts = beats.split("x")
+    println(parts)
+    // the sound file is played when there is no pause ("") => sound
+    // or at the end of a pause "-" => pause, sound
     var count = 0
     for (part in parts) {
         count += part.length + 1
         if (part == "") {
             playSound(file)
-        }
-        else {
-            Thread.sleep(100 * (part.length + 1L))
+        } else {
+            // "delay" only suspends coroutine, not the whole thread
+            delay(100 * (part.length + 1L))
+            //Thread.sleep(100 * (part.length + 1L))
             if (count < beats.length) {
                 playSound(file)
+                // so it plays the file anyway???
             }
         }
     }
@@ -31,12 +45,19 @@ fun playSound(file: String) {
     clip.start()
 }
 
-fun main() {
-    val userDir = System.getProperty("user.dir")
-    println("user.dir: $userDir")
+// main has to have this keyword so that it may call playBeats(), which
+// in turn is suspendable and will be paused when it calls sleep()
+suspend fun main() {
+    // val userDir = System.getProperty("user.dir")
+    // println("user.dir: $userDir")
 
-    GlobalScope.launch {
-    playBeats("x--x--x--x--x--x-", "toms.aiff")
+    runBlocking {
+        // here, the coroutine runs in the same thread, not in
+        // a separate thread (as with "globalScope.launch")
+        // coroutines in the same thread cannot run simultaneously
+        // (HFKotlin p406)
+        // [isn't that the sense of coroutines in the first place?]
+        launch { playBeats("x---x-x-x----x", "toms.aiff") }
+        //playBeats("x-----x-----", "crash_cymbal.aiff")
     }
-    playBeats("x--------x-------", "crash_cymbal.aiff")
 }
